da5c9c5a060d16a42c293f70dd9b44ccac48e0f2
Lionel T Lynch <49052535+LionelxLynch@users.noreply.github.com>
2023-03-30 16:12:27 -0400

    Delete leet_code directory

--- leet_code/TwoSums.py
+++
@@ -1,9 +0,0 @@
-
-class Solution(object):
-    #def twoSum(self, nums, target):
-    nums = [1,2,3,4,5,6,7];
-    #self.target = target;
-       
-
-    for next_index in range(len(nums)): 
-       
