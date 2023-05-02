c7fbc5add9cea585cf7a48766143294dc4248333
Lionel T Lynch <49052535+LionelxLynch@users.noreply.github.com>
2023-05-02 06:13:52 -0400

    Update BinarySearch.java
    Added recursive approach

--- Data Structures and Algorithms/algorithms/BinarySearch.java
+++ Data Structures and Algorithms/algorithms/BinarySearch.java
@@ -1,6 +1,7 @@
 package com.lioneltlynch.algorithms.algorithms;
 
 public class BinarySearch {
+    // Iterative approach 
     public static int binarySearch(int[] arr, int key) {
     int low = 0;
     int high = arr.length - 1;
@@ -20,5 +21,31 @@ public class BinarySearch {
     // The key is not in the array
     return -1;
 }
+    
+    // Overloaded recursive function 
+    public static int binarySearchRecursive(int[] arr, int key) {
+        return binarySearchRecursive(arr, key, 0, arr.length - 1);
+}
 
+    // Recursive approach 
+    public static int binarySearchRecursive(int[] arr, int key, int low, int high) {
+        // Base case 
+        if (low > high) {
+            // The key is not in the array
+            return -1;
+        }
+        int mid = (low + high) / 2;
+        if (arr[mid] == key) {
+            // Found the key at index mid
+            return mid;
+        } 
+        else if (arr[mid] < key) {
+            // Key is in the right half of the array
+            return binarySearchRecursive(arr, key, mid + 1, high);
+        } 
+        else {
+            // Key is in the left half of the array
+            return binarySearchRecursive(arr, key, low, mid - 1);
+        }
+    }
 }
